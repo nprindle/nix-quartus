bcd9c727b89ad783823b6ff3935aa34b7e112bdf
nprindle <nprindle18@gmail.com>
2020-01-10 16:18:13 -0500

    Minor cleanup

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,18 +1,12 @@
-let fetcher = { owner
-              , repo
-              , rev
-              , sha256
-              }:
-    builtins.fetchTarball
-    {
-        inherit sha256;
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+let
+  fetchNixpkgs = { owner, repo, rev, sha256 }:
+    builtins.fetchTarball {
+      url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+      inherit sha256;
     };
-    nixpkgs-src = fetcher
-    {
-        owner = "nixos";
-        repo = "nixpkgs-channels";
-        rev = "95fed28ac372c61eb83c87ad97c24b0f957827bf";
-        sha256 = "03jdb28khdm45gzwl7wvcb7h10yb6y45s7ds8bhlfk9a8phzj4hx";
-    };
-in import nixpkgs-src {}
+in fetchNixpkgs {
+  owner = "nixos";
+  repo = "nixpkgs-channels";
+  rev = "95fed28ac372c61eb83c87ad97c24b0f957827bf";
+  sha256 = "03jdb28khdm45gzwl7wvcb7h10yb6y45s7ds8bhlfk9a8phzj4hx";
+}
